a82cdb3841bc80257fe92a256b65878b5b4e287a
ACUno90 <159036403+ACUno90@users.noreply.github.com>
2024-10-04 12:32:06 -0400

    Saved my UI as a prefab

---
+++ Ruin Hunters/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance { get; private set; }
+
+    [SerializeField] private List<Item> items = new List<Item>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); //keep inventory manager acdross scenes
+        }
+        else
+        {
+            Destroy(gameObject); // no duplicates
+        }
+    }
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public List<Item> GetItems()
+    {
+        return items;
+    }
+
+}
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public string description;
+    public int effectAmount; // For example, healing amount
+    public int amountOfItem;
+    public bool damageable;
+    public PublicEnums.Effects potionEffect;
+}
