7b3e18cda5fc08aad60b8801c21196cb0d2a413d
Jacob Malpica <jacobmalpica@gmail.com>
2024-10-09 12:43:02 -0400

    frame work 1 for the desert elite

--- Ruin Hunters/Assets/Scripts/Enemy/EnemyAI.cs
+++ Ruin Hunters/Assets/Scripts/Enemy/EnemyAI.cs
@@ -302,9 +302,9 @@ public class EnemyAI : MonoBehaviour
         enemyModel.transform.position = postionOG;
         GameManager.Instance.EndTurn();
     }
-    private void Elite_AI()
+    private void Elite_AI() // for eliets and bosses 
     {
-        if(ty== PublicEnums.EnemyTypes.Elite_forest_1)
+        if(ty== PublicEnums.EnemyTypes.Elite_forest_1) // forest elite will be a blue cloaked assassin 
         {
             if(enemyStats.special_count == 0)
             {
@@ -313,29 +313,254 @@ public class EnemyAI : MonoBehaviour
             }
             if (enemyStats.special == true)
             {
+                Skill chosenSkill = availableSkills[0];
+
+
+                enemyStats.special = false;
+            }
+            else
+            {
+                Skill chosenSkill = availableSkills[Random.Range(1, availableSkills.Count)];
+            }
+            
+        }
+        if (ty == PublicEnums.EnemyTypes.Elite_dessert_1) // desert elite will be an ice wolf 
+        {
+            if (enemyStats.special_count == 0) // at the start he will houl 
+            {
+               
+                if (enemyStats.special == false) // this will change the day time dessert to a full moon blizzard 
+                {
+                    enemyStats.special_count = 4;
+                    float weaponMultiplier = GetWeaponMultiplier(PublicEnums.WeaponType.Sword);
+                    availableSkills[5].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance); // this clenses all his effects
+                    availableSkills[0].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance); // and gives him an attack buff 
+                    for (int i = 0; i < GameManager.Instance.battleParty.Count; i++) // then will do an aoe on the party doing ice dmg 
+                    {
+                        GameObject target = null;
+                        target = GameManager.Instance.battleParty[i];
+                        if (target.GetComponent<playerController>().playerStats.health <= 0)
+                        {
+                            targetIndicatorE.transform.position = target.transform.position;
+                            targetIndicatorE.transform.position = new Vector3(targetIndicatorE.transform.position.x, targetIndicatorE.transform.position.y + 9, targetIndicatorE.transform.position.x);
+                            targetIndicatorE.SetActive(true);
+                            StartCoroutine(combatpause());
+                            if (target != null)
+                            {
+                                // Calculate skill damage using any multipliers
+                                float multiplier = GetSkillMultiplier(availableSkills[3].elementType);
+
+                                // Activate the skill, passing the player as the target
+                                availableSkills[3].ActivateSkill(target, enemyStats.attackDamage, multiplier, enemyStats.critChance, enemyStats.effectChance); // Attacker power is set to 10 for now
+                                targetIndicatorE.SetActive(false);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+                    enemyStats.special_count++;
+                    enemyStats.special = true;
+                }
+                if (enemyStats.special == true) // if the full moon is out it will change to a new moon the blizzard will stop 
+                {
+                    enemyStats.special_count = 4;
+                    float weaponMultiplier = GetWeaponMultiplier(PublicEnums.WeaponType.Sword);
+                    availableSkills[5].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance); // he will clense his debuffs 
+                    availableSkills[2].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance); // and heal himself 
+                    for (int i = 0; i < GameManager.Instance.battleParty.Count; i++) // and will do an aoe that lowers the defence of the party this will then switch imbetween both for the remainder of the fight
+                    {
+                        GameObject target = null;
+                        target = GameManager.Instance.battleParty[i];
+                        if (target.GetComponent<playerController>().playerStats.health <= 0)
+                        {
+                            targetIndicatorE.transform.position = target.transform.position;
+                            targetIndicatorE.transform.position = new Vector3(targetIndicatorE.transform.position.x, targetIndicatorE.transform.position.y + 9, targetIndicatorE.transform.position.x);
+                            targetIndicatorE.SetActive(true);
+                            StartCoroutine(combatpause());
+                            if (target != null)
+                            {
+                                // Calculate skill damage using any multipliers
+                                float multiplier = GetSkillMultiplier(availableSkills[4].elementType);
+
+                                // Activate the skill, passing the player as the target
+                                availableSkills[2].ActivateSkill(target, enemyStats.attackDamage, multiplier, enemyStats.critChance, enemyStats.effectChance); // Attacker power is set to 10 for now
+                                targetIndicatorE.SetActive(false);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+                    enemyStats.special_count++;
+                    enemyStats.special = false;
+                }
+
 
             }
-            Skill chosenSkill = availableSkills[Random.Range(0, availableSkills.Count)];
-        }
-        if (ty == PublicEnums.EnemyTypes.Elite_dessert_1)
-        {
-            if (enemyStats.special_count == 0)
+            if (enemyStats.health < enemyStats.maxHealthOG/2) // under 50% hp he attacks 2 times
             {
-                enemyStats.special_count = 4;
+                int ran2;
+                ran2 = Random.Range(0, 100);
+                if (ran2 < 0)
+                {
+                    ran2 = 1;
+                }
+                if (ran2 < 55)
+                {
+                    Skill chosenSkill2 = availableSkills[Random.Range(6, availableSkills.Count)];
+                    if (chosenSkill2.Ptargit == 1)
+                    {
+
+
+                        UseSupportSkill(chosenSkill2);
+
+
+                    }
+                    if (chosenSkill2.Ptargit == 0)
+                    {
+
+                        UseAttackSkill(chosenSkill2);
+
+                    }
+                }
+                else
+                {
+                    // Perform a basic attack
+                    PerformBasicAttack();
+                }
+            }
+            enemyStats.special_count--;
+            int ran;
+            ran = Random.Range(0, 100);
+            if (ran < 0)
+            {
+                ran = 1;
+            }
+            if (ran < 55)
+            {
+                Skill chosenSkill = availableSkills[Random.Range(6, availableSkills.Count)];
+                if (chosenSkill.Ptargit == 1)
+                {
+
+
+                    UseSupportSkill(chosenSkill);
+
+
+                }
+                if (chosenSkill.Ptargit == 0)
+                {
+
+                    UseAttackSkill(chosenSkill);
+
+                }
+            }
+            else
+            {
+                // Perform a basic attack
+                PerformBasicAttack();
+            }
+        }
+        if (ty == PublicEnums.EnemyTypes.Elite_ice_1) // ice elite will be a steam mariachi bot
+        {
+            if (enemyStats.special_count == 0) // this will start at 1 but at 0 he will clense his debuffs from his explosive finish
+            {
+
+                float weaponMultiplier = GetWeaponMultiplier(PublicEnums.WeaponType.Sword);
+                availableSkills[3].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance);
+                enemyStats.special_count++;
+            }
+           
+                if (enemyStats.special_count == 5) // at 5 he over clocks doing 4 basic attacks then he explodes doing masive dmg to the whole party
+            {
+                float weaponMultiplier = GetWeaponMultiplier(PublicEnums.WeaponType.Sword);
+                PerformBasicAttack();
+                PerformBasicAttack();
+                PerformBasicAttack();
+                PerformBasicAttack();
+                for (int i = 0; i < GameManager.Instance.battleParty.Count; i++)
+                {
+                    GameObject target = null;
+                    target = GameManager.Instance.battleParty[i];
+                    if (target.GetComponent<playerController>().playerStats.health <= 0)
+                    {
+                        targetIndicatorE.transform.position = target.transform.position;
+                        targetIndicatorE.transform.position = new Vector3(targetIndicatorE.transform.position.x, targetIndicatorE.transform.position.y + 9, targetIndicatorE.transform.position.x);
+                        targetIndicatorE.SetActive(true);
+                        StartCoroutine(combatpause());
+                        if (target != null)
+                        {
+                            // Calculate skill damage using any multipliers
+                            float multiplier = GetSkillMultiplier(availableSkills[3].elementType);
+
+                            // Activate the skill, passing the player as the target
+                            availableSkills[2].ActivateSkill(target, enemyStats.attackDamage, multiplier, enemyStats.critChance, enemyStats.effectChance); // Attacker power is set to 10 for now
+                            targetIndicatorE.SetActive(false);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                enemyStats.special_count = 0;
+
+
                 enemyStats.special = true;
             }
-            Skill chosenSkill = availableSkills[Random.Range(0, availableSkills.Count)];
-        }
-        if (ty == PublicEnums.EnemyTypes.Elite_ice_1)
-        {
-            if (enemyStats.special_count == 0)
+                if (enemyStats.special == true) // after that tho he loses defence and speed
             {
-                enemyStats.special_count = 2;
-                enemyStats.special = true;
+                Skill chosenSkill = availableSkills[0];
+                float weaponMultiplier = GetWeaponMultiplier(PublicEnums.WeaponType.Sword);
+                availableSkills[0].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance);
+                availableSkills[1].ActivateSkill(GameManager.Instance.enemyObj[0], enemyStats.attackDamage, weaponMultiplier, enemyStats.critChance, enemyStats.effectChance);
+
+                enemyStats.special = false;
             }
-            Skill chosenSkill = availableSkills[Random.Range(0, availableSkills.Count)];
+            else if(enemyStats.special_count != 5 | enemyStats.special_count != 0) // normaly tho he has a 50/50 chance to use a skill or normal attack
+            {
+                enemyStats.special_count++;
+                int ran;
+                ran = Random.Range(0, 100);
+                if (ran < 0)
+                {
+                    ran = 1;
+                }
+                if (ran < 55)
+                {
+                    Skill chosenSkill = availableSkills[Random.Range(4, availableSkills.Count)];
+                    if (chosenSkill.Ptargit == 1)
+                    {
+
+
+                        UseSupportSkill(chosenSkill);
+
+
+                    }
+                    if (chosenSkill.Ptargit == 0)
+                    {
+
+                        UseAttackSkill(chosenSkill);
+
+                    }
+                }
+                else
+                {
+                    // Perform a basic attack
+                    PerformBasicAttack();
+                }
+
+            }
+           
+              
+            
         }
-        if (ty == PublicEnums.EnemyTypes.Elite_ruin_1)
+        if (ty == PublicEnums.EnemyTypes.Elite_ruin_1) // ruin elite will be a booster worrior  
         {
             if (enemyStats.special_count == 0)
             {
